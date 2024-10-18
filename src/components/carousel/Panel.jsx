c87a2e0a6495f1ab345146179af09fcf28032b3e
bug0630 <bugbdh@gmail.com>
2024-10-18 20:04:49 +0900

    pagination fix

--- src/components/carousel/Panel.jsx
+++ src/components/carousel/Panel.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "./panel.scss";
 
 export default React.forwardRef(({ index, imageSrc }, ref) => (
   <div className="panel" ref={ref}>
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import Header from "../components/header/Header";
 import { useMediaQuery } from "react-responsive";
-import Flicking from "@egjs/react-flicking";
+import Flicking, { ViewportSlot } from "@egjs/react-flicking";
+import { AutoPlay } from "@egjs/flicking-plugins";
+import { Pagination } from "@egjs/flicking-plugins";
 import "@egjs/react-flicking/dist/flicking.css";
 import Panel from "../components/carousel/Panel";
 import "./main.scss";
@@ -9,30 +11,54 @@ import "./main.scss";
 export default function Main() {
   const isSmallScreen = useMediaQuery({ query: "(max-width: 786px)" });
   const images = require.context("../assets/img", false, /\.(png|jpe?g|svg)$/);
-
-  // 이미지 이름을 배열로 정의
-  const bannerNames = [
-    { default: "banner1.png", small: "banner1-2.png" },
-    { default: "banner2.png", small: "banner2-2.png" },
-    { default: "banner3.png", small: "banner3-2.png" },
+  const plugins = [
+    new AutoPlay({
+      duration: 5000,
+      direction: "NEXT",
+      stopOnHover: false,
+      animationDuration: 1000,
+    }),
+    new Pagination({ type: "fraction" }),
   ];
-  const banners = bannerNames.map((banner) => ({
-    default: images(`./${banner.default}`),
-    small: images(`./${banner.small}`),
-  }));
+  const defaultBanners = [
+    images("./banner1.png"),
+    images("./banner2.png"),
+    images("./banner3.png"),
+  ];
+
+  const smallBanners = [
+    images("./banner1-2.png"),
+    images("./banner2-2.png"),
+    images("./banner3-2.png"),
+  ];
 
   return (
     <div className="main">
       <Header />
-      <Flicking align="prev" circular={true}>
-        {banners.map((banner, index) => (
-          <Panel
-            key={index}
-            index={index}
-            imageSrc={isSmallScreen ? banner.small : banner.default}
-          />
-        ))}
-      </Flicking>
+      {isSmallScreen ? (
+        <Flicking plugins={plugins} align="prev" circular={true}>
+          {smallBanners.map((banner, index) => (
+            <Panel key={index} index={index} imageSrc={banner} />
+          ))}
+          <ViewportSlot>
+            <div className="flicking-pagination"></div>
+          </ViewportSlot>
+        </Flicking>
+      ) : (
+        ""
+      )}
+      {isSmallScreen ? (
+        ""
+      ) : (
+        <Flicking plugins={plugins} align="prev" circular={true}>
+          {defaultBanners.map((banner, index) => (
+            <Panel key={index} index={index} imageSrc={banner} />
+          ))}
+          <ViewportSlot>
+            <div className="flicking-pagination"></div>
+          </ViewportSlot>
+        </Flicking>
+      )}
     </div>
   );
 }
