76c5d8adfbebbf131cbf42063408b4a596f2b2b2
bug0630 <bugbdh@gmail.com>
2024-11-08 21:14:03 +0900

    a

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../components/header/Header";
 import HeaderNav from "../components/header/HeaderNav";
 import { useMediaQuery } from "react-responsive";
+import { useState, useEffect } from "react";
 import List from "../components/List/List";
 import Flicking, { ViewportSlot } from "@egjs/react-flicking";
 import { AutoPlay, Arrow, Pagination } from "@egjs/flicking-plugins";
@@ -16,25 +17,40 @@ import Tab from "../components/tab/Tab";
 export default function Main() {
   const smallScreen = useMediaQuery({ query: "(max-width: 786px)" });
   const images = require.context("../assets/img", false, /\.(png|jpe?g|svg)$/);
-  const plugins = [
-    new AutoPlay({
+  const [plugins, setPlugins] = useState([]);
+  const [productPlugins1, setProductPlugins1] = useState([]);
+  const [productPlugins2, setProductPlugins2] = useState([]);
+
+  useEffect(() => {
+    const bannerAutoPlay = new AutoPlay({
       duration: 5000,
       direction: "NEXT",
       stopOnHover: false,
       animationDuration: 1000,
-    }),
-    new Pagination({ type: "bullet" }),
-    new Arrow(),
-  ];
-  const productPlugins = [
-    new AutoPlay({
+    });
+
+    const productAutoPlay1 = new AutoPlay({
       duration: 5000,
       direction: "NEXT",
       stopOnHover: false,
       animationDuration: 0,
-    }),
-    new Arrow(),
-  ];
+    });
+
+    const productAutoPlay2 = new AutoPlay({
+      duration: 5000,
+      direction: "NEXT",
+      stopOnHover: false,
+      animationDuration: 0,
+    });
+
+    setPlugins([
+      bannerAutoPlay,
+      new Pagination({ type: "bullet" }),
+      new Arrow(),
+    ]);
+    setProductPlugins1([productAutoPlay1, new Arrow()]);
+    setProductPlugins2([productAutoPlay2, new Arrow()]);
+  }, []);
   const defaultBanners = [
     images("./banner1.png"),
     images("./banner2.png"),
@@ -51,6 +67,12 @@ export default function Main() {
     images("./product2.png"),
     images("./product3.png"),
     images("./product4.png"),
+  ];
+  const sale = [
+    images("./sale1.png"),
+    images("./sale2.png"),
+    images("./sale3.png"),
+    images("./sale4.png"),
   ];
   const product2 = [images("./product2-1.png"), images("./product2-2.png")];
   return (
@@ -101,7 +123,7 @@ export default function Main() {
         <div className="product">
           <List list={product} path="제품" />
           <Flicking
-            plugins={productPlugins}
+            plugins={productPlugins1}
             duration="0"
             align="prev"
             circular={true}
@@ -127,6 +149,23 @@ export default function Main() {
           <span className="blue">SALE</span> 주간 !
         </h3>
         <Tab list={["오늘만 세일", "이번주 세일"]}></Tab>
+        <Flicking
+          plugins={productPlugins2}
+          duration="0"
+          align="center"
+          circular={true}
+        >
+          {sale.map((product2, index) => (
+            <Panel
+              key={index}
+              path={`/product2-${index + 1}`}
+              imageSrc={product2}
+            />
+          ))}
+          <ViewportSlot>
+            <Control></Control>
+          </ViewportSlot>
+        </Flicking>
       </section>
       <Footer></Footer>
       <BottomNav></BottomNav>
